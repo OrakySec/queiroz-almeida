4a9c19a7b7598867e965c888cd113e9bbdcb35dc
Ykaro Roberto <orakysec@gmail.com>
2026-05-08 20:44:16 -0300

    style: restore rounded corners for navbar logo

--- packages/frontend/components/public/Navbar.tsx
+++ packages/frontend/components/public/Navbar.tsx
@@ -48,6 +48,7 @@ export function Navbar() {
                 }`}
                 style={{ 
                   height: 'auto',
+                  borderRadius: '100px'
                 }} 
               />
             </Link>
