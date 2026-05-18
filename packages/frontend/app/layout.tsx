68575798a50ba13663ea11f4980003a52ab11b34
Ykaro Roberto <orakysec@gmail.com>
2026-05-18 13:42:58 -0300

    feat: adicionar Google Tag Manager no layout global

--- packages/frontend/app/layout.tsx
+++ packages/frontend/app/layout.tsx
@@ -74,8 +74,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="preconnect" href="https://storage.queirozalmeidaconstrutora.com.br" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://api.teste.ykaromarques.com" crossOrigin="anonymous" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5P5L2PHN');`
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-sans antialiased text-brand-texto bg-white selection:bg-brand-marinho/20 selection:text-brand-marinho">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5P5L2PHN"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
